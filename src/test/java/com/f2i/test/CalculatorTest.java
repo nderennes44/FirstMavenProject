be3c4ed603bf476f69afa4ee44e7e45eab424627
nderennes44 <derennes53@yahoo.fr>
2019-10-03 16:54:01 +0200

    initial commit

---
+++ src/test/java/com/f2i/test/CalculatorTest.java
@@ -0,0 +1,22 @@
+package com.f2i.test;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.f2i.Calculator;
+
+public class CalculatorTest {
+
+	Calculator c = new Calculator();
+
+	@Test
+	public void addTest() {
+		Assert.assertEquals(5, c.add(2, 3));
+	}
+
+	@Test
+	public void multiplyTest() {
+		Assert.assertEquals(6, c.multiply(2, 3));
+	}
+
+}
